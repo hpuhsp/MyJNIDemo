ff4cf7a2e5ea8bf21e2c720d81c0b3695d768e3c
ReeseHu <1101121039@qq.com>
2018-03-27 10:18:55 +0800

    nothing

--- app/src/main/java/com/reesehu/jnidemo/JNIUtils.java
+++ app/src/main/java/com/reesehu/jnidemo/JNIUtils.java
@@ -3,7 +3,7 @@ package com.reesehu.jnidemo;
 /**
  * Function:
  * <br/>
- * Describe:
+ * Describe: 测试native方法
  * <br/>
  * Author: reese on 2018/3/8.
  * <br/>
@@ -13,8 +13,10 @@ package com.reesehu.jnidemo;
 public class JNIUtils {
     public JNIUtils() {
     }
+
     static {
         System.loadLibrary("hello");
     }
+
     public static native String getStringFromC();
 }
